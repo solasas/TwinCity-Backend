3734d793ba870bbd041deb4452cb779e5a41ca88
solasas <sashanksolasa@gmail.com>
2026-08-03 23:41:46 +0530

    initialized project

---
+++ src/main/java/com/sashank/DigitalTwinBackend/DigitalTwinBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sashank.DigitalTwinBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalTwinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalTwinBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sashank/DigitalTwinBackend/DigitalTwinBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sashank.DigitalTwinBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalTwinBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
